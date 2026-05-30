183cbcdfc65ca7de53717df95c84192a949d842a
m12ano <asiapallemoscie@gmail.com>
2026-05-30 10:26:30 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Service Worker minimale per abilitare l'installazione PWA su Chrome
+self.addEventListener('fetch', (event) => {
+    // Lascia che le richieste passino normalmente alla rete
+});
