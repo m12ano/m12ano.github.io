7249c14bde29bf1bc84a676d25886419fc233ac6
m12ano <asiapallemoscie@gmail.com>
2026-05-30 10:42:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,41 @@
-// Service Worker minimale per abilitare l'installazione PWA su Chrome
-self.addEventListener('fetch', (event) => {
-    // Lascia che le richieste passino normalmente alla rete
+const CACHE_NAME = 'm12ano-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Installa il Service Worker e salva in cache i file principali
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Attiva e pulisci vecchie cache
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Rispondi dalle cache o vai in rete
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
 });
