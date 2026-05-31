9d15d1ec877d2de52ac2054e587db3b2a2311247
m12ano <asiapallemoscie@gmail.com>
2026-05-31 18:20:23 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,33 @@
-const CACHE_NAME = 'm12ano-v1';
+const CACHE_NAME = 'purpl3l3an-cache-v2'; // <--- Incrementa questo (v1, v2, v3...) ad ogni update
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  // tutti gli altri tuoi asset...
 ];
 
-// Installa il Service Worker e salva in cache i file principali
+// Installazione - Salva la nuova cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    }).then(() => self.skipWaiting())
+    })
   );
 });
 
-// Attiva e pulisci vecchie cache
+// Attivazione - Elimina le vecchie cache obsolete
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
         keys.map((key) => {
           if (key !== CACHE_NAME) {
+            console.log('Vecchia cache eliminata:', key);
             return caches.delete(key);
           }
         })
       );
-    }).then(() => self.clients.claim())
-  );
-});
-
-// Rispondi dalle cache o vai in rete
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
     })
   );
 });
